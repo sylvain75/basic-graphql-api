34201b839206af0574854a1b237d2a4875eb7184
Sylvain <sylvoz75@gmail.com>
2020-02-18 22:03:41 +1100

    init

---
+++ index.js
@@ -0,0 +1,58 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+
+
+const typeDefs = gql`
+  enum Status {
+    WATCHED
+    INTERESTED
+    NOT_INTERESTED
+    UNKNOWN
+  }
+
+  type Movie {
+    id: ID
+    title: String
+    releaseDate: String
+    rating: Int
+    status: Status
+  }
+
+  type Query {
+    movies: [Movie]
+    movie(id: ID): Movie
+  }
+`
+
+const movies = [
+  {
+    id: 'one',
+    title: 'five deadly venoms',
+    releaseDate: "1983",
+    rating: 5
+  },
+  {
+    id: 'two',
+    title: '36 chamber',
+    releaseDate: "1985",
+    rating: 5
+  }
+];
+
+const resolvers = {
+  Query: {
+    movies: () => {
+      return movies;
+    },
+    movie: (obj, { id }, context, info) => {
+      const movie = movies.find((movie) => movie.id === id)
+      return movie;
+    }
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server started at ${url}`);
+})
